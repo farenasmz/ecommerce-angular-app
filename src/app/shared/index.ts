5656b24769ff3a21063f3d884cfa990f15ab77ea
farenasmz <farenasmz@gmail.com>
2021-09-03 19:52:50 -0500

    Video 50 finalizado, button genérico

---
+++ src/app/shared/index.ts
@@ -0,0 +1,6 @@
+//Busca el archivo index automáticamente, por eso no es necesario especificar el archivo como tal.
+export * from './buttons';
+export * from './controls';
+export * from './indicators';
+export * from './layouts';
+export * from './popups';
